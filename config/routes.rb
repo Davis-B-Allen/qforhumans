27c4fd6f7cc1b87d70a1d4dd624afc9ea6c00ba6
Davis Allen <davis.b.allen@gmail.com>
2017-08-28 19:40:15 -0400

    Add a deck model and controller to show a deck

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # get 'decks/show'
+  resources :decks, only: [:show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
---
+++ test/controllers/decks_controller_test.rb
@@ -0,0 +1,5 @@
+require 'test_helper'
+
+class DecksControllerTest < ActionDispatch::IntegrationTest
+
+end
