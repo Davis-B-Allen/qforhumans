63eaae38c364727d2061237cbc200c65cb5bbfe0
Davis Allen <davis.b.allen@gmail.com>
2017-08-29 17:53:17 -0400

    Validate uniqueness of lens name for each card in a given deck

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,5 @@
 class Card < ApplicationRecord
   belongs_to :deck
   validates :deck_id, presence: true
+  validates :lens_name, presence: true, uniqueness: { scope: :deck_id }
 end
--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -4,8 +4,7 @@ class CardTest < ActiveSupport::TestCase
 
   def setup
     @deck = decks(:qforhum)
-    @card = Card.new(lens_prefix: "The Lens of", lens_name: "You", deck_id: @deck.id)
-    # @card = @deck.cards.build(lens_prefix: "The Lens of", lens_name: "You")
+    @card = @deck.cards.build(lens_prefix: "The Lens of", lens_name: "You")
   end
 
   test "should be valid" do
@@ -15,5 +14,14 @@ class CardTest < ActiveSupport::TestCase
   test "deck id should be present" do
     @card.deck_id = nil
     assert_not @card.valid?
+  end
+
+  test "lens name should be unique for each card in deck" do
+    duplicate_card = @card.dup
+    other_deck = decks(:one)
+    other_card = Card.new(lens_prefix: "The Lens of", lens_name: "You", deck_id: other_deck.id)
+    @card.save
+    assert_not duplicate_card.valid?
+    assert other_card.valid?
   end
 end
