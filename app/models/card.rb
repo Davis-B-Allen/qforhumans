3b6266bcdbd6272f3d8d52fc40fbe0778d97d0eb
Davis Allen <davis.b.allen@gmail.com>
2017-09-02 14:55:54 -0400

    Apply a default order for cards, ranked by face number and then suit

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,6 @@
 class Card < ApplicationRecord
   belongs_to :deck
+  default_scope -> { order(face_number: :asc, face_suit: :asc) }
   validates :deck_id, presence: true
   validates :lens_name, presence: true, uniqueness: { scope: :deck_id }
 end
