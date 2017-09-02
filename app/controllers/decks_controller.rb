a4013221d540e8f7dd1aff137640af54561ef168
Davis Allen <davis.b.allen@gmail.com>
2017-09-02 14:31:51 -0400

    Route 'cards' link to a featured controller action showing the default deck

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,4 +1,10 @@
 class DecksController < ApplicationController
+
+  def featured
+    @deck = Deck.find(1)
+    render 'show'
+  end
+
   def show
     @deck = Deck.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   # get 'decks/show'
   resources :decks, only: [:show]
   resources :cards, only: [:show]
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/cards', to: 'decks#featured'
+
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
-  get '/cards', to: 'static_pages#cards'
   get '/about', to: 'static_pages#about'
 end
