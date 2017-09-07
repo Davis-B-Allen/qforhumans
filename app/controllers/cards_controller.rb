fbad9e5c3ab35adaae9e4a6911b264293855cb58
Davis Allen <davis.b.allen@gmail.com>
2017-09-07 19:46:36 -0400

    Add a random card button, get rid of default scope on cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,4 +3,14 @@ class CardsController < ApplicationController
   def show
     @card = Card.find(params[:id])
   end
+
+  # Returns a random card
+  # Note that this method uses a database call that is NOT DATABASE AGNOSTIC
+  # "RANDOM()" should work for postgresql and sqlite
+  # Also note that this returns ANY random card, not just any random card belonging to a given deck
+  def random
+    @card = Card.order("RANDOM()").first
+    # Note: Could alternatively redirect to the card show path for the random card
+    render 'show'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
+  get '/random', to: 'cards#random'
 end
