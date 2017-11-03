9e332e15a0a7629cebb69b6d5b93f8a85a951f00
Davis Allen <davis.b.allen@gmail.com>
2017-11-03 14:31:33 -0400

    Rename site Cards Against Inanity and update homepage to include game rules accordion

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Questions for Humans"
+    base_title = "Cards Against Inanity"
     if page_title.empty?
       base_title
     else
